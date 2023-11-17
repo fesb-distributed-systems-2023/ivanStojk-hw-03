e08492df8d3097c4f11df05c6857798fe42af674
ivanStojk <138300375+ivanStojk@users.noreply.github.com>
2023-11-17 10:47:45 +0100

    Uploading model

---
+++ Models/HotelProperties.cs
@@ -0,0 +1,11 @@
+﻿namespace ivanStojk_CRUD_API.Models
+{
+    public class HotelProperties
+    {
+        public int HotelId { get; set; }
+        public string GuestId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string RoomNumber { get; set; }
+    }
+}
