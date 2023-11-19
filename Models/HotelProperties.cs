8fb59ad27cc9614e52947292f74e626e8ecf94b8
ivanStojk <138300375+ivanStojk@users.noreply.github.com>
2023-11-19 10:48:17 +0100

    Promjena Imena

--- Models/HotelProperties.cs
+++ Models/HotelProperties.cs
@@ -1,9 +1,9 @@
 ﻿namespace ivanStojk_CRUD_API.Models
 {
-    public class HotelProperties
+    public class GuestProperties
     {
-        public int HotelId { get; set; }
-        public string GuestId { get; set; }
+        public int Id { get; set; }
+        public string HotelId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string RoomNumber { get; set; }
