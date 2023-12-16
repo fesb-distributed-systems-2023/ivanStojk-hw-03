74528928dea93f905bf93793ec6eda9c49c34f40
ivanStojk <138300375+ivanStojk@users.noreply.github.com>
2023-12-16 11:30:58 +0100

    Adding SQL

---
+++ SQL/CreateGuest.sql
@@ -0,0 +1,5 @@
+INSERT INTO Guest (HotelId, FirstName, LastName, RoomNumber)
+VALUES
+("Park", "Ivan", "Stojković", "321"),
+("Park", "Martin", "Čelar", "222"),
+("Radisson Blu", "Josip", "Sanader", "122");
